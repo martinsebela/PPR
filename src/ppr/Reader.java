40a531792118e7cbb1613dd898c3ddf503d204d6
Lukáš Burian <lukas.burian@avast.com>
2020-06-07 21:15:44 +0200

    some improvements vol.2

--- src/ppr/Reader.java
+++ src/ppr/Reader.java
@@ -1,26 +1,28 @@
 package ppr;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class Reader {
 
     private static final String FILE_NAME = "text.txt";
+    private static final String EMPTY_STRING = " ";
 
-    public static Collection<String[]> readFromFile() throws IOException {
+    public static Collection<WordPair> readFromFile() throws IOException {
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        Collection<String[]> store = new ArrayList<>();
+        Collection<WordPair> store = new ArrayList<>();
 
         try (InputStream input = cl.getResourceAsStream(FILE_NAME)) {
+            if (input == null) {
+                throw new FileNotFoundException(String.format("File %s does't exist.", FILE_NAME));
+            }
             InputStreamReader isr = new InputStreamReader(input);
             BufferedReader br = new BufferedReader(isr);
             String line;
             while ((line = br.readLine()) != null) {
-                store.add(line.split(" "));
+                String[] pair = line.split(EMPTY_STRING);
+                store.add(WordPair.createPair(pair[0], pair[1]));
             }
         }
         return store;
